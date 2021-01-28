c3960ad43afc618e60b82470de6fb5855551c580
Martin Duy Tat <martin.tat@physics.ox.ac.uk>
2021-01-28 23:20:14 +0000

    Added files for declaring my factory

---
+++ src/components/KKpipi_entries.cxx
@@ -0,0 +1,10 @@
+#include "GaudiKernel/DeclareFactoryEntries.h"
+#include "KKpipi/KKpipiSingleTag.h"
+
+DECLARE_ALGORITHM_FACTORY(KKpipiSingleTag)
+
+DECLARE_FACTORY_ENTRIES(KKpipi) {
+  DECLARE_ALGORITHM(KKpipi)
+}
+
+
