2b2e01499d91e1f472b14ac53557697fe0fb0bf9
Martin Duy Tat <martin.tat@physics.ox.ac.uk>
2021-02-11 17:26:20 +0000

    Added header file with documentation for a class FindKKpipiTag which will calculate all variables for a tag mode KKpipi

---
+++ KKpipi/FindKKpipiTagInfo.h
@@ -0,0 +1,193 @@
+// Martin Duy Tat 28th January 2021, based on code by Yu Zhang
+/**
+ * FindKKpipiTag is a class for extracting all the variables of a KKpipi tag
+ */
+
+#ifndef FINDKKPIPITAG
+#define FINDKKPIPITAG
+
+// BOSS
+#include "DTagTool/DTagTool.h"
+// CLHEP
+#include "CLHEP/Vector/LorentzVector.h"
+// STL
+#include<vector>
+#include<string>
+
+class FindKKpipiTag {
+  public: 
+    /**
+     * Default constructor that initalizes all variables to zero
+     */
+    FindKKpipiTag();
+    /**
+     * Trivial destructor
+     */
+    ~FindKKpipiTag();
+    /**
+     * Function that calculates all the tag information and saves them
+     * @param DTTool_iter Iterator pointing to tag candidate
+     * @param DTTool DTagTool object with all the tag information
+     * @param Returns true if successful
+     */
+    bool AssignKKpipiDaughterInfo(DTagToolIterator DTTool_iter, DTagTool &DTTool);
+    /**
+     * Enumeration to label daughter particles in the order K+ K- pi+ pi-
+     */
+    enum DaughterParticle {KPLUS, KMINUS, PIPLUS, PIMINUS};
+    /**
+     * Get \f$K^+\f$ momentum component
+     * @param i Component
+     */
+    double GetKPlusP(int i);
+    /**
+     * Get \f$K^-\f$ momentum component
+     * @param i Component
+     */
+    double GetKMinusP(int i);
+    /**
+     * Get \f$\pi^+\f$ momentum component
+     * @param i Component
+     */
+    double GetPiPlusP(int i);
+    /**
+     * Get \f$\pi^-\f$ momentum component
+     * @param i Component
+     */
+    double GetPiMinusP(int i);
+    /**
+     * Get flag of Kalman fit success
+     */
+    int GetKalmanFitSuccess();
+    /**
+     * Get Kalman fit \f$chi^2\f$
+     */
+    double GetKalmanFitChi2();
+    /**
+     * Get \f$K^+\f$ momentum component from Kalman fit
+     * @param i Component
+     */
+    double GetKPlusPKalmanFit(int i);
+    /**
+     * Get \f$K^-\f$ momentum component from Kalman fit
+     * @param i Component
+     */
+    double GetKMinusPKalmanFit(int i);
+    /**
+     * Get \f$\pi^+\f$ momentum component from Kalman fit
+     * @param i Component
+     */
+    double GetPiPlusPKalmanFit(int i);
+    /**
+     * Get \f$\pi^-\f$ momentum component from Kalman fit
+     * @param i Component
+     */
+    double GetPiMinusPKalmanFit(int i);
+    /**
+     * Get flag equal of \f$K_S^0\f$ fit of tracks
+     */
+    int GetKSFitSuccess();
+    /**
+     * Get the \f$K_S\f$ decay length, from VeeVertexAlg
+     */
+    double GetDecayLengthVeeVertex();
+    /**
+     * Get the \f$K_S^0\f$ \f$\chi^2\f$, from VeeVertexAlg
+     */
+    double GetChi2VeeVertex();
+    /**
+     * Get the \f$K_S^0\f$ mass, from VeeVertexAlg
+     */
+    double GetKSMassVeeVertex();
+    /**
+     * Get the \f$K_S^0\f$ decay length, from fit
+     */
+    double GetDecayLengthFit();
+    /**
+     * Get the \f$K_S^0\f$ decay length error, from fit
+     */
+    double GetDecayLengthErrorFit();
+    /**
+     * Get the \f$K_S^0\f$ \f$\chi^2\f$, from fit of primary vertex
+     */
+    double GetChi2Fit();
+    /**
+     * Get the \f$K_S^0\f$ mass, from fit
+     */
+    double GetKSMassFit();
+  private:
+    /**
+     * \f$K^+\f$ four-momentum
+     */
+    CLHEP::HepLorentzVector m_KPlusP;
+    /**
+     * \f$K^-\f$ four-momentum
+     */
+    CLHEP::HepLorentzVector m_KMinusP;
+    /**
+     * \f$\pi^+\f$ four-momentum
+     */
+    CLHEP::HepLorentzVector m_PiPlusP;
+    /**
+     * \f$\pi^-\f$ four-momentum
+     */
+    CLHEP::HepLorentzVector m_PiMinusP;
+    /**
+     * Flag equal to 1 for success and 0 for fail in the Kalman fit of tracks
+     */
+    int m_KalmanFitSuccess = 0;
+    /**
+     * \f$\chi^2\f$ of Kalman fit
+     */
+    double m_KalmanFitChi2 = 0;
+    /**
+     * Kalman fitted \f$K^+\f$ four- momentum
+     */
+    CLHEP::HepLorentzVector m_KPlusPKalmanFit;
+    /**
+     * Kalman fitted \f$K^-\f$ four-momentum
+     */
+    CLHEP::HepLorentzVector m_KMinuspxKalmanFit;
+    /**
+     * Kalman fitted \f$\pi^+\f$ four-momentum
+     */
+    CLHEP::HepLorentzVector m_PiPlusPKalmanFit;
+    /**
+     * Kalman fitted \f$\pi^-\f$ four-momentum
+     */
+    CLHEP::HepLorentzVector m_PiMinusPKalmanFit;
+    /**
+     * Flag equal to 1 for success and 0 for fail in the \f$K_S^0\f$ fit of tracks
+     */
+    int m_KSFitSuccess = 0;
+    /**
+     * The \f$K_S\f$ decay length, from VeeVertexAlg
+     */
+    double m_DecayLengthVeeVertex = 0;
+    /**
+     * The \f$K_S^0\f$ \f$\chi^2\f$, from VeeVertexAlg
+     */
+    double m_Chi2VeeVertex = 0;
+    /**
+     * The \f$K_S^0\f$ mass, from VeeVertexAlg
+     */
+    double m_KSMassVeeVertex = 0;
+    /**
+     * The \f$K_S^0\f$ decay length, from fit
+     */
+    double m_DecayLengthFit = 0;
+    /**
+     * The \f$K_S^0\f$ decay length error, from fit
+     */
+    double m_DecayLengthErrorFit = 0;
+    /**
+     * The \f$K_S^0\f$ \f$\chi^2\f$, from fit of primary vertex
+     */
+    double m_Chi2Fit = 0;
+    /**
+     * The \f$K_S^0\f$ mass, from fit
+     */
+    double m_KSMassFit = 0;
+};
+
+#endif
