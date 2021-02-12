03b2fee59125fa2e57f7f738948df3e6bacd4496
Martin Duy Tat <martin.tat@physics.ox.ac.uk>
2021-02-12 17:15:25 +0000

    Added the algorithm to BOSS

--- src/components/KKpipi_entries.cxx
+++ src/components/KKpipi_entries.cxx
@@ -1,9 +1,11 @@
 #include "GaudiKernel/DeclareFactoryEntries.h"
 #include "KKpipi/KKpipiSingleTag.h"
 #include "KKpipi/KKpipiVersusKpiDoubleTag.h"
+#include "KKpipi/KKpipiVersusKpipi0DoubleTag.h"
 
 DECLARE_ALGORITHM_FACTORY(KKpipiSingleTag)
 DECLARE_ALGORITHM_FACTORY(KKpipiVersusKpiDoubleTag)
+DECLARE_ALGORITHM_FACTORY(KKpipiVersusKpipi0DoubleTag)
 
 DECLARE_FACTORY_ENTRIES(KKpipi) {
   DECLARE_ALGORITHM(KKpipi)
