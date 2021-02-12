0872b00fd79d343b9313cf034fd6aff8520f80f3
Martin Duy Tat <martin.tat@physics.ox.ac.uk>
2021-02-12 22:18:52 +0000

    Added a job options file for KKpipi vs KSpi0 and added the algorithms to BOSS

--- src/components/KKpipi_entries.cxx
+++ src/components/KKpipi_entries.cxx
@@ -3,11 +3,13 @@
 #include "KKpipi/KKpipiVersusKpiDoubleTag.h"
 #include "KKpipi/KKpipiVersusKpipi0DoubleTag.h"
 #include "KKpipi/KKpipiVersusKKDoubleTag.h"
+#include "KKpipi/KKpipiVersusKSpi0DoubleTag.h"
 
 DECLARE_ALGORITHM_FACTORY(KKpipiSingleTag)
 DECLARE_ALGORITHM_FACTORY(KKpipiVersusKpiDoubleTag)
 DECLARE_ALGORITHM_FACTORY(KKpipiVersusKpipi0DoubleTag)
 DECLARE_ALGORITHM_FACTORY(KKpipiVersusKKDoubleTag)
+DECLARE_ALGORITHM_FACTORY(KKpipiVersusKSpi0DoubleTag)
 
 DECLARE_FACTORY_ENTRIES(KKpipi) {
   DECLARE_ALGORITHM(KKpipi)
