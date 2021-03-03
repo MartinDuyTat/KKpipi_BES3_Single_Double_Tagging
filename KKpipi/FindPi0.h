04d37c42a870219c59edc5f3320749578d16b90c
Martin Duy Tat <martin.tat@physics.ox.ac.uk>
2021-03-03 17:38:34 +0000

    Changed FindPi0 so that it can now find an arbitrary number of pi0

--- KKpipi/FindPi0.h
+++ KKpipi/FindPi0.h
@@ -1,6 +1,7 @@
 // Martin Duy Tat 12th February 2021, based on code by Yu Zhang
 /**
  * FindPi0 is class for finding \f$\pi^0\f$ candidates and returning the unconstrained and kinematically constrained four-momenta of the photons
+ * By default one \f$\pi^0\f$ is assumed, unless otherwise is specified in the constructor and the getters
  */
 
 #ifndef FINDPI0
@@ -11,13 +12,16 @@
 #include "DTagTool/DTagTool.h"
 // CLHEP
 #include "CLHEP/Vector/LorentzVector.h"
+// STL
+#include <vector>
 
 class FindPi0 {
   public: 
     /**
-     * Default constructor, initializes everything to zero
+     * Default constructor, initializes all properties to zero and the number of \f$\pi^0\f$ to one
+     * @param npi0 Number of \f$\pi^0\f$ to look for
      */
-    FindPi0();
+    FindPi0(int npi0 = 1);
     /**
      * Trivial destructor
      */
@@ -38,48 +42,57 @@ class FindPi0 {
     /** 
      * Get the high energy photon unconstrained four-momentum
      * @param i Momentum component
+     * @param pi0_index 0 for first \f$\pi^0\f$, 1 for second \f$\pi^0\f$...
      */
-    double GetHighEPhotonP(int i) const;
+    double GetHighEPhotonP(int i, int pi0_index = 0) const;
     /** 
      * Get the high energy photon unconstrained four-momentum
      * @param i Momentum component
+     * @param pi0_index 0 for first \f$\pi^0\f$, 1 for second \f$\pi^0\f$...
      */
-    double GetLowEPhotonP(int i) const;
+    double GetLowEPhotonP(int i, int pi0_index = 0) const;
     /** 
      * Get the high energy photon kinematically constrained four-momentum
      * @param i Momentum component
+     * @param pi0_index 0 for first \f$\pi^0\f$, 1 for second \f$\pi^0\f$...
      */
-    double GetHighEPhotonPConstrained(int i) const;
+    double GetHighEPhotonPConstrained(int i, int pi0_index = 0) const;
     /** 
      * Get the low energy photon kinematically constrained four-momentum
      * @param i Momentum component
+     * @param pi0_index 0 for first \f$\pi^0\f$, 1 for second \f$\pi^0\f$...
      */
-    double GetLowEPhotonPConstrained(int i) const;
+    double GetLowEPhotonPConstrained(int i, int pi0_index = 0) const;
     /**
      * Get the \f$\chi^2\f$ of the kinematic fit
+     * @param pi0_index 0 for first \f$\pi^0\f$, 1 for second \f$\pi^0\f$...
      */
-    double GetChi2Fit() const;
+    double GetChi2Fit(int pi0_index = 0) const;
   private:
     /**
      * The high energy photon unconstrained four-momentum
      */
-    CLHEP::HepLorentzVector m_HighEPhotonP;
+    std::vector<CLHEP::HepLorentzVector> m_HighEPhotonP;
     /**
      * The low energy photon unconstrained four-momentum
      */
-    CLHEP::HepLorentzVector m_LowEPhotonP;
+    std::vector<CLHEP::HepLorentzVector> m_LowEPhotonP;
     /**
      * The high energy photon kinematically constrained four-momentum
      */
-    CLHEP::HepLorentzVector m_HighEPhotonPConstrained;
+    std::vector<CLHEP::HepLorentzVector> m_HighEPhotonPConstrained;
     /**
      * The high energy photon kinematically constrained four-momentum
      */
-    CLHEP::HepLorentzVector m_LowEPhotonPConstrained;
+    std::vector<CLHEP::HepLorentzVector> m_LowEPhotonPConstrained;
     /**
      * Kinematic fit \f$\chi^2\f$
      */
-    double m_Chi2Fit;
+    std::vector<double> m_Chi2Fit;
+    /**
+     * Number of \f$\pi^0\f$ to look for
+     */
+    int m_npi0;
 };
 
 #endif
--- src/FindPi0.cxx
+++ src/FindPi0.cxx
@@ -38,7 +38,12 @@
 // Particle masses
 #include "KKpipi/ParticleMasses.h"
 
-FindPi0::FindPi0(): m_Chi2Fit(0.0) {
+FindPi0::FindPi0(int npi0): m_Chi2Fit(0.0), m_npi0(npi0) {
+  m_HighEPhotonP.resize(m_npi0);
+  m_LowEPhoton.resize(m_npi0);
+  m_HighEPhotonPConstrained.resize(m_npi0);
+  m_LowEPhotonConstrained.resize(m_npi0);
+  m_Chi2Fit.resize(m_npi0);
 }
 
 FindPi0::~FindPi0() {
@@ -65,6 +70,7 @@ StatusCode FindPi0::findPi0(DTagToolIterator &DTTool_iter, DTagTool &DTTool) {
   // Get the track ID of the tagged pi0 candidates
   std::vector<int> Pi0TrackIDs = DTTool.pi0Id(DTTool_iter, 1);
   // Loop over all pi0 candidates found by EvtRecPi0
+  int i = 0;
   for(EvtRecPi0Col::iterator Pi0_iter = RecPi0Col->begin(); Pi0_iter != RecPi0Col->end(); Pi0_iter++) {
     // Check if pi0 candidate from the DTagTool is the same as the candidate found by EvtRecPi0
     if(Pi0_iter - RecPi0Col->begin() != Pi0TrackIDs[0]) {
@@ -73,34 +79,37 @@ StatusCode FindPi0::findPi0(DTagToolIterator &DTTool_iter, DTagTool &DTTool) {
     // Get EM shower four-momenta of photons
     RecEmcShower *HighEPhotonShower = const_cast<EvtRecTrack*>((*Pi0_iter)->hiEnGamma())->emcShower();
     RecEmcShower *LowEPhotonShower = const_cast<EvtRecTrack*>((*Pi0_iter)->loEnGamma())->emcShower();
-    m_HighEPhotonP = GetPhoton4Vector(HighEPhotonShower->energy(), HighEPhotonShower->theta(), HighEPhotonShower->phi());
-    m_LowEPhotonP = GetPhoton4Vector(LowEPhotonShower->energy(), LowEPhotonShower->theta(), LowEPhotonShower->phi());
+    m_HighEPhotonP[i] = GetPhoton4Vector(HighEPhotonShower->energy(), HighEPhotonShower->theta(), HighEPhotonShower->phi());
+    m_LowEPhotonP[i] = GetPhoton4Vector(LowEPhotonShower->energy(), LowEPhotonShower->theta(), LowEPhotonShower->phi());
     // Get kinematically constrained four-momenta of photons
-    m_HighEPhotonPConstrained = (*Pi0_iter)->hiPfit();
-    m_LowEPhotonPConstrained = (*Pi0_iter)->loPfit();
-    m_Chi2Fit = (*Pi0_iter)->chisq();
-    return StatusCode::SUCCESS;
+    m_HighEPhotonPConstrained[i] = (*Pi0_iter)->hiPfit();
+    m_LowEPhotonPConstrained[i] = (*Pi0_iter)->loPfit();
+    m_Chi2Fit[i] = (*Pi0_iter)->chisq();
+    i++;
+    if(i == m_npi0) {
+      return StatusCode::SUCCESS;
+    }
   }
-  log << "Could not find any matching pi0 candidates" << endreq;
+  log << "Could not find the requested number of pi0 candidates" << endreq;
   return StatusCode::FAILURE;
 }
 
-double FindPi0::GetHighEPhotonP(int i) const {
-  return m_HighEPhotonP[i];
+double FindPi0::GetHighEPhotonP(int i, int pi0_index) const {
+  return m_HighEPhotonP[pi0_index][i];
 }
 
-double FindPi0::GetLowEPhotonP(int i) const {
-  return m_LowEPhotonP[i];
+double FindPi0::GetLowEPhotonP(int i, int pi0_index) const {
+  return m_LowEPhotonP[pi0_index][i];
 }
 
-double FindPi0::GetHighEPhotonPConstrained(int i) const {
-  return m_HighEPhotonPConstrained[i];
+double FindPi0::GetHighEPhotonPConstrained(int i, int pi0_index) const {
+  return m_HighEPhotonPConstrained[pi0_index][i];
 }
 
-double FindPi0::GetLowEPhotonPConstrained(int i) const {
-  return m_LowEPhotonPConstrained[i];
+double FindPi0::GetLowEPhotonPConstrained(int i, int pi0_index) const {
+  return m_LowEPhotonPConstrained[pi0_index][i];
 }
 
-double FindPi0::GetChi2Fit() const {
-  return m_Chi2Fit;
+double FindPi0::GetChi2Fit(int pi0_index) const {
+  return m_Chi2Fit[pi0_index];
 }
