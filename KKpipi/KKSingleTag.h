0ddb8c4816628f74f057a2bd8d3ee493c84abd87
Martin Duy Tat <martin.tat@physics.ox.ac.uk>
2021-03-12 13:33:05 +0000

    Added KK single tag

---
+++ KKpipi/KKSingleTag.h
@@ -0,0 +1,165 @@
+// Martin Duy Tat 12th March 2021
+/**
+ * KKSingleTag is a class for a BOSS algorithm
+ * It runs over \f$D^0\bar{D^0}\f$ data and saves all events with a single \f$D\to K^+K^-\f$ tag
+ */
+
+#ifndef KKSINGLETAG
+#define KKSINGLETAG
+
+// Gaudi
+#include "GaudiKernel/AlgFactory.h"
+#include "GaudiKernel/Algorithm.h"
+#include "GaudiKernel/NTuple.h"
+#include "GaudiKernel/StatusCode.h"
+// BOSS
+#include "DTagTool/DTagTool.h"
+// STL
+#include<string>
+
+class KKSingleTag: public Algorithm {
+  public: 
+    /**
+     * Default constructor for an algorithm where all necessary properties are declared
+     */
+    KKSingleTag(const std::string& name, ISvcLocator* pSvcLocator);
+    /**
+     * Trivial destructor
+     */
+    ~KKSingleTag();
+    /**
+     * This function runs when algorithm is initialized
+     */
+    StatusCode initialize();
+    /**
+     * Execution of the algorithm
+     */
+    StatusCode execute();
+    /**
+     * This function runs when algorithm is finalized
+     */
+    StatusCode finalize();
+    /**
+     * Helper function to fill in information about the tag mode
+     * @param DTTool_iter Iterator pointing to single tag candidate
+     * @param DTTool DTagTool object with all the event information
+     */
+    StatusCode FillTuple(DTagToolIterator DTTool_iter, DTagTool &DTTool);
+  private:
+    /**
+     * Dummy variable, placeholder for more important properties to be added later
+     */
+    int m_dummy;
+    /**
+     * The NTuple itself that is filled
+     */
+    NTuple::Tuple *m_tuple;
+    /**
+     * Run number (negative for MC event)
+     */
+    NTuple::Item<int> m_RunNumber;
+    /**
+     * Event number
+     */
+    NTuple::Item<int> m_EventNumber;
+    /**
+     * Number of particles in the decay chain
+     * For example, \f$D^0\to K^-\pi^+\f$ counts as \f$3\f$ particles
+     */
+    NTuple::Item<int> m_NumberParticles;
+    /**
+     * Array of particle IDs of all particles in the decay chain
+     */
+    NTuple::Array<int> m_pdgID;
+    /**
+     * Array of indices referring to the particle mother
+     * For example, an index 0 means that ```m_pdgID[0]``` is the mother
+     */
+    NTuple::Array<int> m_MotherIndex;
+    /**
+     * Generator label of the decay mode
+     */
+    NTuple::Item<int> m_MCmode;
+    /**
+     * Array of true x momenta
+     */
+    NTuple::Array<double> m_TruePx;
+    /**
+     * Array of true y momenta
+     */
+    NTuple::Array<double> m_TruePy;
+    /**
+     * Array of true z momenta
+     */
+    NTuple::Array<double> m_TruePz;
+    /**
+     * Array of true energies
+     */
+    NTuple::Array<double> m_TrueEnergy;
+    /**
+     * Invariant mass of \f$D\f$ meson
+     */
+    NTuple::Item<double> m_DMass;
+    /**
+     * Beam constrained mass
+     */
+    NTuple::Item<double> m_MBC;
+    /**
+     * \f$E_D - E_{\text{beam}}\f$
+     */
+    NTuple::Item<double> m_DeltaE;
+    /**
+     * Beam energy
+     */
+    NTuple::Item<double> m_BeamE;
+    /**
+     * \f$D\f$ momentum along \f$x\f$
+     */
+    NTuple::Item<double> m_Dpx;
+    /**
+     * \f$D\f$ momentum along \f$y\f$
+     */
+    NTuple::Item<double> m_Dpy;
+    /**
+     * \f$D\f$ momentum along \f$z\f$
+     */
+    NTuple::Item<double> m_Dpz;
+    /**
+     * \f$D\f$ energy
+     */
+    NTuple::Item<double> m_Denergy;
+    /**
+     * \f$K^+\f$ momentum along \f$x\f$
+     */
+    NTuple::Item<double> m_KPluspx;
+    /**
+     * \f$K^+\f$ momentum along \f$y\f$
+     */
+    NTuple::Item<double> m_KPluspy;
+    /**
+     * \f$K^+\f$ momentum along \f$z\f$
+     */
+    NTuple::Item<double> m_KPluspz;
+    /**
+     * \f$K^+\f$ energy
+     */
+    NTuple::Item<double> m_KPlusenergy;
+    /**
+     * \f$K^-\f$ momentum along \f$x\f$
+     */
+    NTuple::Item<double> m_KMinuspx;
+    /**
+     * \f$K^-\f$ momentum along \f$y\f$
+     */
+    NTuple::Item<double> m_KMinuspy;
+    /**
+     * \f$K^-\f$ momentum along \f$z\f$
+     */
+    NTuple::Item<double> m_KMinuspz;
+    /**
+     * \f$K^-\f$ energy
+     */
+    NTuple::Item<double> m_KMinusenergy;
+};
+
+#endif
---
+++ src/KKSingleTag.cxx
@@ -0,0 +1,171 @@
+// Martin Duy Tat 12th March 2021
+
+// KKpipi
+#include "KKpipi/KKSingleTag.h"
+#include "KKpipi/FindhhTagInfo.h"
+#include "KKpipi/FindMCInfo.h"
+// Gaudi
+#include "GaudiKernel/AlgFactory.h"
+#include "GaudiKernel/Bootstrap.h"
+#include "GaudiKernel/IDataProviderSvc.h"
+#include "GaudiKernel/MsgStream.h"
+#include "GaudiKernel/IHistogramSvc.h"
+#include "GaudiKernel/INTupleSvc.h"
+#include "GaudiKernel/ISvcLocator.h"
+#include "GaudiKernel/MsgStream.h"
+#include "GaudiKernel/NTuple.h"
+#include "GaudiKernel/PropertyMgr.h"
+#include "GaudiKernel/SmartDataPtr.h"
+#include "GaudiKernel/SmartRefVector.h"
+// Event information
+#include "EventModel/Event.h"
+#include "EventModel/EventModel.h"
+#include "EventModel/EventHeader.h"
+#include "EvtRecEvent/EvtRecEvent.h"
+#include "EvtRecEvent/EvtRecTrack.h"
+#include "EvtRecEvent/EvtRecDTag.h"
+// CLHEP
+#include "CLHEP/Vector/LorentzVector.h"
+// Boss
+#include "DTagTool/DTagTool.h"
+#include "McDecayModeSvc/McDecayModeSvc.h"
+#include "McTruth/McParticle.h"
+// STL
+#include<vector>
+#include<string>
+
+KKSingleTag::KKSingleTag(const std::string &name, ISvcLocator *pSvcLocator): Algorithm(name, pSvcLocator) {
+  declareProperty("dummy", m_dummy = 0);
+}
+
+KKSingleTag::~KKSingleTag() {
+}
+
+StatusCode KKSingleTag::initialize() {
+  MsgStream log(msgSvc(), name());
+  log << MSG::INFO << "Initializing KK Single Tagging" << endreq;
+  StatusCode status;
+  NTuplePtr ntp(ntupleSvc(), "KKPIPI/SingleTag");
+  if(ntp) {
+    m_tuple = ntp;
+  } else {
+    m_tuple = ntupleSvc()->book("KKPIPI/SingleTag", CLID_ColumnWiseTuple, "Single tagged D->KK events");
+    if(m_tuple) {
+      status = m_tuple->addItem("Run", m_RunNumber);
+      status = m_tuple->addItem("Event", m_EventNumber);
+      status = m_tuple->addItem("NumberOfParticles", m_NumberParticles, 0, 100);
+      status = m_tuple->addIndexedItem("ParticleIDs", m_NumberParticles, m_pdgID);
+      status = m_tuple->addIndexedItem("MotherIndex", m_NumberParticles, m_MotherIndex);
+      status = m_tuple->addItem("MCmode", m_MCmode);
+      status = m_tuple->addIndexedItem("True_Px", m_NumberParticles, m_TruePx);
+      status = m_tuple->addIndexedItem("True_Py", m_NumberParticles, m_TruePy);
+      status = m_tuple->addIndexedItem("True_Pz", m_NumberParticles, m_TruePz);
+      status = m_tuple->addIndexedItem("True_Energy", m_NumberParticles, m_TrueEnergy);
+      status = m_tuple->addItem("DMass", m_DMass);
+      status = m_tuple->addItem("MBC", m_MBC);
+      status = m_tuple->addItem("DeltaE", m_DeltaE);
+      status = m_tuple->addItem("BeamE", m_BeamE);
+      status = m_tuple->addItem("Dpx", m_Dpx);
+      status = m_tuple->addItem("Dpy", m_Dpy);
+      status = m_tuple->addItem("Dpz", m_Dpz);
+      status = m_tuple->addItem("Denergy", m_Denergy);
+      status = m_tuple->addItem("KPluspx", m_KPluspx);
+      status = m_tuple->addItem("KPluspy", m_KPluspy);
+      status = m_tuple->addItem("KPluspz", m_KPluspz);
+      status = m_tuple->addItem("KPlusenergy", m_KPlusenergy);
+      status = m_tuple->addItem("KMinuspx", m_KMinuspx);
+      status = m_tuple->addItem("KMinuspy", m_KMinuspy);
+      status = m_tuple->addItem("KMinuspz", m_KMinuspz);
+      status = m_tuple->addItem("KMinusenergy", m_KMinusenergy);
+    } else {
+      log << MSG::ERROR << "Cannot book NTuple for KK Single Tags" << endmsg;
+      return StatusCode::FAILURE;
+    }
+    return StatusCode::SUCCESS;
+  }
+}
+
+StatusCode KKpipiSingleTag::execute() {
+  MsgStream log(msgSvc(), name());
+  log << MSG::INFO << "Executing KK Single Tag Algorithm" << endreq;
+  SmartDataPtr<Event::EventHeader> eventHeader(eventSvc(), "/Event/EventHeader");
+  m_RunNumber = eventHeader->runNumber();
+  m_EventNumber = eventHeader->eventNumber();
+  DTagTool DTTool;
+  DTTool.setPID(true);
+  if(DTTool.isDTagListEmpty()) {
+    log << MSG::DEBUG << "No D candidates found" << endreq;
+    return StatusCode::SUCCESS;
+  }
+  if(!DTTool.cosmicandleptonVeto()) {
+    log << MSG::DEBUG << "Cosmic and lepton veto" << endreq;
+    return StatusCode::SUCCESS;
+  }
+  if(DTTool.findSTag(EvtRecDTag::kD0toKK)) {
+    DTagToolIterator DTTool_iter = DTTool.stag();
+    StatusCode FillTupleStatus = FillTuple(DTTool_iter, DTTool);
+    if(FillTupleStatus != StatusCode::SUCCESS) {
+        log << MSG::FATAL << "Assigning tuple info failed" << endreq;
+	return StatusCode::FAILURE;
+    }
+    m_tuple->write();
+  }
+  return StatusCode::SUCCESS;
+}
+
+StatusCode KKpipiSingleTag::finalize() {
+  MsgStream log(msgSvc(), name());
+  log << MSG::INFO << "Finalizing KK Single Tagging" << endreq;
+  return StatusCode::SUCCESS;
+}
+
+StatusCode KKpipiSingleTag::FillTuple(DTagToolIterator DTTool_iter, DTagTool &DTTool) {
+  if(m_RunNumber < 0) {
+    SmartDataPtr<Event::McParticleCol> MCParticleCol(eventSvc(), "/Event/MC/McParticleCol");
+    if(!MCParticleCol) {
+      return StatusCode::FAILURE;
+    }
+    IMcDecayModeSvc *IMcDecayModeService;
+    StatusCode McDecayModeSVC_Status = service("McDecayModeSvc", IMcDecayModeService);
+    if(McDecayModeSVC_Status.isFailure()) {
+      return StatusCode::FAILURE;
+    }
+    FindMCInfo findMCInfo;
+    StatusCode MCStatus = findMCInfo.CalculateMCInfo(MCParticleCol, IMcDecayModeService);
+    if(MCStatus != StatusCode::SUCCESS) {
+      return MCStatus;
+    }
+    m_NumberParticles = findMCInfo.GetNumberParticles();
+    m_MCmode = findMCInfo.GetMCmode();
+    for(int i = 0; i < m_NumberParticles; i++) {
+      m_pdgID[i] = findMCInfo.GetpdgID(i);
+      m_MotherIndex[i] = findMCInfo.GetMotherIndex(i);
+      m_TruePx[i] = findMCInfo.GetTruePx(i);
+      m_TruePy[i] = findMCInfo.GetTruePy(i);
+      m_TruePz[i] = findMCInfo.GetTruePz(i);
+      m_TrueEnergy[i] = findMCInfo.GetTrueEnergy(i);
+    }
+  }
+  m_DMass = (*DTTool_iter)->mass();
+  m_MBC = (*DTTool_iter)->mBC();
+  m_DeltaE = (*DTTool_iter)->deltaE();
+  m_BeamE = (*DTTool_iter)->beamE();
+  m_Dpx = (*DTTool_iter)->p4().x();
+  m_Dpy = (*DTTool_iter)->p4().y();
+  m_Dpz = (*DTTool_iter)->p4().z();
+  m_Denergy = (*DTTool_iter)->p4().t();
+  FindhhTagInfo findKKTagInfo("KK");
+  StatusCode status = findKKTagInfo.CalculateTagInfo(DTTool_iter, DTTool);
+  if(status != StatusCode::SUCCESS) {
+    return status;
+  }
+  m_KPluspx = findKKTagInfo.GethPlusP(0);
+  m_KPluspy = findKKTagInfo.GethPlusP(1);
+  m_KPluspz = findKKTagInfo.GethPlusP(2);
+  m_KPlusenergy = findKKTagInfo.GethPlusP(3);
+  m_KMinuspx = findKKTagInfo.GethMinusP(0);
+  m_KMinuspy = findKKTagInfo.GethMinusP(1);
+  m_KMinuspz = findKKTagInfo.GethMinusP(2);
+  m_KMinusenergy = findKKTagInfo.GethMinusP(3);
+  return StatusCode::SUCCESS;
+}
