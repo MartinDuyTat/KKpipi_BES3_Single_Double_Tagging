4470b563b21f42519b5499861e64f805fc838890
Martin Duy Tat <martin.tat@physics.ox.ac.uk>
2021-03-12 18:10:50 +0000

    Added the KSpipi single tag

---
+++ KKpipi/KSpipiSingleTag.h
@@ -0,0 +1,361 @@
+// Martin Duy Tat 12th March 2021
+/**
+ * KSpipiSingleTag is a class for a BOSS algorithm
+ * It runs over \f$D^0\bar{D^0}\f$ data and saves all events with a single \f$D\to K_S^0\pi^+\pi^-\f$ tag
+ */
+
+#ifndef KSPIPISINGLETAG
+#define KSPIPISINGLETAG
+
+// Gaudi
+#include "GaudiKernel/AlgFactory.h"
+#include "GaudiKernel/Algorithm.h"
+#include "GaudiKernel/NTuple.h"
+#include "GaudiKernel/StatusCode.h"
+// BOSS
+#include "DTagTool/DTagTool.h"
+// STL
+#include<string>
+
+class KSpipiSingleTag: public Algorithm {
+  public: 
+    /**
+     * Default constructor for an algorithm where all necessary properties are declared
+     */
+    KSpipiSingleTag(const std::string& name, ISvcLocator* pSvcLocator);
+    /**
+     * Trivial destructor
+     */
+    ~KSpipiSingleTag();
+    /**
+     * This function runs when algorithm is initialized
+     */
+    StatusCode initialize();
+    /**
+     * Execution of the algorithm
+     */
+    StatusCode execute();
+    /**
+     * This function runs when algorithm is finalized
+     */
+    StatusCode finalize();
+    /**
+     * Helper function to fill in information about the tag mode
+     * @param DTTool_iter Iterator pointing to single tag candidate
+     * @param DTTool DTagTool object with all the event information
+     */
+    StatusCode FillTuple(DTagToolIterator DTTool_iter, DTagTool &DTTool);
+  private:
+    /**
+     * Dummy variable, placeholder for more important properties to be added later
+     */
+    int m_dummy;
+    /**
+     * The NTuple itself that is filled
+     */
+    NTuple::Tuple *m_tuple;
+    /**
+     * Run number (negative for MC event)
+     */
+    NTuple::Item<int> m_RunNumber;
+    /**
+     * Event number
+     */
+    NTuple::Item<int> m_EventNumber;
+    /**
+     * Number of particles in the decay chain
+     * For example, \f$D^0\to K^-\pi^+\f$ counts as \f$3\f$ particles
+     */
+    NTuple::Item<int> m_NumberParticles;
+    /**
+     * Array of particle IDs of all particles in the decay chain
+     */
+    NTuple::Array<int> m_pdgID;
+    /**
+     * Array of indices referring to the particle mother
+     * For example, an index 0 means that ```m_pdgID[0]``` is the mother
+     */
+    NTuple::Array<int> m_MotherIndex;
+    /**
+     * Generator label of the decay mode
+     */
+    NTuple::Item<int> m_MCmode;
+    /**
+     * Array of true x momenta
+     */
+    NTuple::Array<double> m_TruePx;
+    /**
+     * Array of true y momenta
+     */
+    NTuple::Array<double> m_TruePy;
+    /**
+     * Array of true z momenta
+     */
+    NTuple::Array<double> m_TruePz;
+    /**
+     * Array of true energies
+     */
+    NTuple::Array<double> m_TrueEnergy;
+    /**
+     * Invariant mass of \f$D\f$ meson
+     */
+    NTuple::Item<double> m_DMass;
+    /**
+     * Beam constrained mass
+     */
+    NTuple::Item<double> m_MBC;
+    /**
+     * \f$E_D - E_{\text{beam}}\f$
+     */
+    NTuple::Item<double> m_DeltaE;
+    /**
+     * Beam energy
+     */
+    NTuple::Item<double> m_BeamE;
+    /**
+     * \f$D\f$ momentum along \f$x\f$
+     */
+    NTuple::Item<double> m_Dpx;
+    /**
+     * \f$D\f$ momentum along \f$y\f$
+     */
+    NTuple::Item<double> m_Dpy;
+    /**
+     * \f$D\f$ momentum along \f$z\f$
+     */
+    NTuple::Item<double> m_Dpz;
+    /**
+     * \f$D\f$ energy
+     */
+    NTuple::Item<double> m_Denergy;
+    /**
+     * The \f$K_S\f$ decay length, from VeeVertexAlg
+     */
+    NTuple::Item<double> m_DecayLengthVeeVertex;
+    /**
+     * The \f$K_S^0\f$ \f$\chi^2\f$, from VeeVertexAlg
+     */
+    NTuple::Item<double> m_Chi2VeeVertex;
+    /**
+     * The \f$K_S^0\f$ mass, from VeeVertexAlg
+     */
+    NTuple::Item<double> m_KSMassVeeVertex;
+    /**
+     * The \f$K_S^0\f$ decay length, from fit
+     */
+    NTuple::Item<double> m_DecayLengthFit;
+    /**
+     * The \f$K_S^0\f$ decay length error, from fit
+     */
+    NTuple::Item<double> m_DecayLengthErrorFit;
+    /**
+     * The \f$K_S^0\f$ \f$\chi^2\f$, from fit of primary vertex
+     */
+    NTuple::Item<double> m_Chi2Fit;
+    /**
+     * The \f$K_S^0\f$ mass, from fit
+     */
+    NTuple::Item<double> m_KSMassFit;
+    /**
+     * \f$K_S^0\f$ momentum along \f$x\f$ after vertex fit
+     */
+    NTuple::Item<double> m_KSpx;
+    /**
+     * \f$K_S^0\f$ momentum along \f$y\f$ after vertex fit
+     */
+    NTuple::Item<double> m_KSpy;
+    /**
+     * \f$K_S^0\f$ momentum along \f$z\f$ after vertex fit
+     */
+    NTuple::Item<double> m_KSpz;
+    /**
+     * \f$K_S^0\f$ energy after vertex fit
+     */
+    NTuple::Item<double> m_KSenergy;
+    /**
+     * The \f$\pi^+\f$ daughter momentum along \f$x\f$ from the MDC track
+     */
+    NTuple::Item<double> m_KSPiPluspx;
+    /**
+     * The \f$\pi^+\f$ daughter momentum along \f$y\f$ from the MDC track
+     */
+    NTuple::Item<double> m_KSPiPluspy;
+    /**
+     * The \f$\pi^+\f$ daughter momentum along \f$z\f$ from the MDC track
+     */
+    NTuple::Item<double> m_KSPiPluspz;
+    /**
+     * The \f$\pi^+\f$ daughter energy from the MDC track
+     */
+    NTuple::Item<double> m_KSPiPlusenergy;
+    /**
+     * The \f$\pi^-\f$ daughter momentum along \f$x\f$ from the MDC track
+     */
+    NTuple::Item<double> m_KSPiMinuspx;
+    /**
+     * The \f$\pi^-\f$ daughter momentum along \f$y\f$ from the MDC track
+     */
+    NTuple::Item<double> m_KSPiMinuspy;
+    /**
+     * The \f$\pi^-\f$ daughter momentum along \f$z\f$ from the MDC track
+     */
+    NTuple::Item<double> m_KSPiMinuspz;
+    /**
+     * The \f$\pi^-\f$ daughter energy from the MDC track
+     */
+    NTuple::Item<double> m_KSPiMinusenergy;
+    /**
+     * The \f$\pi^+\f$ daughter momentum along \f$x\f$ from the MDC track
+     */
+    NTuple::Item<double> m_KSPiPluspxFit;
+    /**
+     * The \f$\pi^+\f$ daughter momentum along \f$y\f$ after vertex fit
+     */
+    NTuple::Item<double> m_KSPiPluspyFit;
+    /**
+     * The \f$\pi^+\f$ daughter momentum along \f$z\f$ after vertex fit
+     */
+    NTuple::Item<double> m_KSPiPluspzFit;
+    /**
+     * The \f$\pi^+\f$ daughter energy after vertex fit
+     */
+    NTuple::Item<double> m_KSPiPlusenergyFit;
+    /**
+     * The \f$\pi^-\f$ daughter momentum along \f$x\f$ after vertex fit
+     */
+    NTuple::Item<double> m_KSPiMinuspxFit;
+    /**
+     * The \f$\pi^-\f$ daughter momentum along \f$y\f$ after vertex fit
+     */
+    NTuple::Item<double> m_KSPiMinuspyFit;
+    /**
+     * The \f$\pi^-\f$ daughter momentum along \f$z\f$ after vertex fit
+     */
+    NTuple::Item<double> m_KSPiMinuspzFit;
+    /**
+     * The \f$\pi^-\f$ daughter energy after vertex fit
+     */
+    NTuple::Item<double> m_KSPiMinusenergyFit;
+    /**
+     * \f$\pi^+\f$ momentum along \f$x\f$
+     */
+    NTuple::Item<double> m_PiPluspx;
+    /**
+     * \f$\pi^+\f$ momentum along \f$y\f$
+     */
+    NTuple::Item<double> m_PiPluspy;
+    /**
+     * \f$\pi^+\f$ momentum along \f$z\f$
+     */
+    NTuple::Item<double> m_PiPluspz;
+    /**
+     * \f$\pi^+\f$ energy
+     */
+    NTuple::Item<double> m_PiPlusenergy;
+    /**
+     * \f$\pi^-\f$ momentum along \f$x\f$
+     */
+    NTuple::Item<double> m_PiMinuspx;
+    /**
+     * \f$\pi^-\f$ momentum along \f$y\f$
+     */
+    NTuple::Item<double> m_PiMinuspy;
+    /**
+     * \f$\pi^-\f$ momentum along \f$z\f$
+     */
+    NTuple::Item<double> m_PiMinuspz;
+    /**
+     * \f$\pi^-\f$ energy
+     */
+    NTuple::Item<double> m_PiMinusenergy;
+    /**
+     * Flag equal to 1 for success and 0 for fail in the Kalman fit of tag tracks
+     */
+    NTuple::Item<int> m_KalmanFitSuccess;
+    /**
+     * \f$\chi^2\f$ of Kalman fit
+     */
+    NTuple::Item<double> m_KalmanFitChi2;
+    /**
+     * Kalman fitted \f$\pi^+\f$ momentum along \f$x\f$
+     */
+    NTuple::Item<double> m_PiPluspxKalmanFit;
+    /**
+     * Kalman fitted \f$\pi^+\f$ momentum along \f$y\f$
+     */
+    NTuple::Item<double> m_PiPluspyKalmanFit;
+    /**
+     * Kalman fitted \f$\pi^+\f$ momentum along \f$z\f$
+     */
+    NTuple::Item<double> m_PiPluspzKalmanFit;
+    /**
+     * Kalman fitted \f$\pi^+\f$ energy
+     */
+    NTuple::Item<double> m_PiPlusenergyKalmanFit;
+    /**
+     * Kalman fitted \f$\pi^-\f$ momentum along \f$x\f$
+     */
+    NTuple::Item<double> m_PiMinuspxKalmanFit;
+    /**
+     * Kalman fitted \f$\pi^-\f$ momentum along \f$y\f$
+     */
+    NTuple::Item<double> m_PiMinuspyKalmanFit;
+    /**
+     * Kalman fitted \f$\pi^-\f$ momentum along \f$z\f$
+     */
+    NTuple::Item<double> m_PiMinuspzKalmanFit;
+    /**
+     * Kalman fitted \f$\pi^-\f$ energy
+     */
+    NTuple::Item<double> m_PiMinusenergyKalmanFit;
+    /**
+     * Kalman fitted \f$K_S^0\f$ momentum along \f$x\f$
+     */
+    NTuple::Item<double> m_KSpxKalmanFit;
+    /**
+     * Kalman fitted \f$K_S^0\f$ momentum along \f$y\f$
+     */
+    NTuple::Item<double> m_KSpyKalmanFit;
+    /**
+     * Kalman fitted \f$K_S^0\f$ momentum along \f$z\f$
+     */
+    NTuple::Item<double> m_KSpzKalmanFit;
+    /**
+     * Kalman fitted \f$K_S^0\f$ energy
+     */
+    NTuple::Item<double> m_KSenergyKalmanFit;
+    /**
+     * Flag equal to 1 for success and 0 for fail in the \f$K_S^0\f$ fit of tag tracks
+     */
+    NTuple::Item<int> m_pipiKSFitSuccess;
+    /**
+     * \f$K_S\f$ decay length, from VeeVertexAlg
+     */
+    NTuple::Item<double> m_pipiDecayLengthVeeVertex;
+    /**
+     * \f$K_S^0\f$ \f$\chi^2\f$, from VeeVertexAlg
+     */
+    NTuple::Item<double> m_pipiChi2VeeVertex;
+    /**
+     * \f$K_S^0\f$ mass, from VeeVertexAlg
+     */
+    NTuple::Item<double> m_pipiKSMassVeeVertex;
+    /**
+     * \f$K_S^0\f$ decay length, from fit
+     */
+    NTuple::Item<double> m_pipiDecayLengthFit;
+    /**
+     * \f$K_S^0\f$ decay length error, from fit
+     */
+    NTuple::Item<double> m_pipiDecayLengthErrorFit;
+    /**
+     * \f$K_S^0\f$ \f$\chi^2\f$, from fit of primary vertex
+     */
+    NTuple::Item<double> m_pipiChi2Fit;
+    /**
+     * \f$K_S^0\f$ mass, from fit
+     */
+    NTuple::Item<double> m_pipiKSMassFit;
+};
+
+#endif
---
+++ src/KSpipiSingleTag.cxx
@@ -0,0 +1,272 @@
+// Martin Duy Tat 12th March 2021
+
+// KKpipi
+#include "KKpipi/KSpipiSingleTag.h"
+#include "KKpipi/FindKSpipiTagInfo.h"
+#include "KKpipi/FindMCInfo.h"
+// Gaudi
+#include "GaudiKernel/AlgFactory.h"
+#include "GaudiKernel/Bootstrap.h"
+#include "GaudiKernel/IDataProviderSvc.h"
+#include "GaudiKernel/MsgStream.h"
+#include "GaudiKernel/IHistogramSvc.h"
+#include "GaudiKernel/INTupleSvc.h"
+#include "GaudiKernel/ISvcLocator.h"
+#include "GaudiKernel/MsgStream.h"
+#include "GaudiKernel/NTuple.h"
+#include "GaudiKernel/PropertyMgr.h"
+#include "GaudiKernel/SmartDataPtr.h"
+#include "GaudiKernel/SmartRefVector.h"
+// Event information
+#include "EventModel/Event.h"
+#include "EventModel/EventModel.h"
+#include "EventModel/EventHeader.h"
+#include "EvtRecEvent/EvtRecEvent.h"
+#include "EvtRecEvent/EvtRecTrack.h"
+#include "EvtRecEvent/EvtRecDTag.h"
+// CLHEP
+#include "CLHEP/Vector/LorentzVector.h"
+// Boss
+#include "DTagTool/DTagTool.h"
+#include "McDecayModeSvc/McDecayModeSvc.h"
+#include "McTruth/McParticle.h"
+// STL
+#include<vector>
+#include<string>
+
+KSpipiSingleTag::KSpipiSingleTag(const std::string &name, ISvcLocator *pSvcLocator): Algorithm(name, pSvcLocator) {
+  declareProperty("dummy", m_dummy = 0);
+}
+
+KSpipiSingleTag::~KSpipiSingleTag() {
+}
+
+StatusCode KSpipiSingleTag::initialize() {
+  MsgStream log(msgSvc(), name());
+  log << MSG::INFO << "Initializing KSpipi Single Tagging" << endreq;
+  StatusCode status;
+  NTuplePtr ntp(ntupleSvc(), "KKPIPI/KSpipiSingleTag");
+  if(ntp) {
+    m_tuple = ntp;
+  } else {
+    m_tuple = ntupleSvc()->book("KKPIPI/KSpipiSingleTag", CLID_ColumnWiseTuple, "Single tagged D->KSpipi events");
+    if(m_tuple) {
+      status = m_tuple->addItem("Run", m_RunNumber);
+      status = m_tuple->addItem("Event", m_EventNumber);
+      status = m_tuple->addItem("NumberOfParticles", m_NumberParticles, 0, 100);
+      status = m_tuple->addIndexedItem("ParticleIDs", m_NumberParticles, m_pdgID);
+      status = m_tuple->addIndexedItem("MotherIndex", m_NumberParticles, m_MotherIndex);
+      status = m_tuple->addItem("MCmode", m_MCmode);
+      status = m_tuple->addIndexedItem("True_Px", m_NumberParticles, m_TruePx);
+      status = m_tuple->addIndexedItem("True_Py", m_NumberParticles, m_TruePy);
+      status = m_tuple->addIndexedItem("True_Pz", m_NumberParticles, m_TruePz);
+      status = m_tuple->addIndexedItem("True_Energy", m_NumberParticles, m_TrueEnergy);
+      status = m_tuple->addItem("DMass", m_DMass);
+      status = m_tuple->addItem("MBC", m_MBC);
+      status = m_tuple->addItem("DeltaE", m_DeltaE);
+      status = m_tuple->addItem("BeamE", m_BeamE);
+      status = m_tuple->addItem("Dpx", m_Dpx);
+      status = m_tuple->addItem("Dpy", m_Dpy);
+      status = m_tuple->addItem("Dpz", m_Dpz);
+      status = m_tuple->addItem("Denergy", m_Denergy);
+      status = m_tuple->addItem("KSDecayLengthVeeVertex", m_DecayLengthVeeVertex);
+      status = m_tuple->addItem("KSChi2VeeVertex", m_Chi2VeeVertex);
+      status = m_tuple->addItem("KSMassVeeVertex", m_KSMassVeeVertex);
+      status = m_tuple->addItem("KSDecayLengthFit", m_DecayLengthFit);
+      status = m_tuple->addItem("KSDecayLengthErrorFit", m_DecayLengthErrorFit);
+      status = m_tuple->addItem("KSChi2Fit", m_Chi2Fit);
+      status = m_tuple->addItem("KSMassFit", m_KSMassFit);
+      status = m_tuple->addItem("KSpx", m_KSpx);
+      status = m_tuple->addItem("KSpy", m_KSpy);
+      status = m_tuple->addItem("KSz", m_KSpz);
+      status = m_tuple->addItem("KSenergy", m_KSenergy);
+      status = m_tuple->addItem("KSPiPluspx", m_KSPiPluspx);
+      status = m_tuple->addItem("KSPiPluspy", m_KSPiPluspy);
+      status = m_tuple->addItem("KSPiPluspz", m_KSPiPluspz);
+      status = m_tuple->addItem("KSPiPlusenergy", m_KSPiPlusenergy);
+      status = m_tuple->addItem("KSPiMinuspx", m_KSPiMinuspx);
+      status = m_tuple->addItem("KSPiMinuspy", m_KSPiMinuspy);
+      status = m_tuple->addItem("KSPiMinuspz", m_KSPiMinuspz);
+      status = m_tuple->addItem("KSPiMinusenergy", m_KSPiMinusenergy);
+      status = m_tuple->addItem("KSPiPluspxFit", m_KSPiPluspxFit);
+      status = m_tuple->addItem("KSPiPluspyFit", m_KSPiPluspyFit);
+      status = m_tuple->addItem("KSPiPluspzFit", m_KSPiPluspzFit);
+      status = m_tuple->addItem("KSPiPlusenergyFit", m_KSPiPlusenergyFit);
+      status = m_tuple->addItem("KSPiMinuspxFit", m_KSPiMinuspxFit);
+      status = m_tuple->addItem("KSPiMinuspyFit", m_KSPiMinuspyFit);
+      status = m_tuple->addItem("KSPiMinuspzFit", m_KSPiMinuspzFit);
+      status = m_tuple->addItem("KSPiMinusenergyFit", m_KSPiMinusenergyFit);
+      status = m_tuple->addItem("PiPluspx", m_PiPluspx);
+      status = m_tuple->addItem("PiPluspy", m_PiPluspy);
+      status = m_tuple->addItem("PiPluspz", m_PiPluspz);
+      status = m_tuple->addItem("PiPlusenergy", m_PiPlusenergy);
+      status = m_tuple->addItem("PiMinuspx", m_PiMinuspx);
+      status = m_tuple->addItem("PiMinuspy", m_PiMinuspy);
+      status = m_tuple->addItem("PiMinuspz", m_PiMinuspz);
+      status = m_tuple->addItem("PiMinusenergy", m_PiMinusenergy);
+      status = m_tuple->addItem("KalmanFitSuccess", m_KalmanFitSuccess);
+      status = m_tuple->addItem("KalmanFitChi2", m_KalmanFitChi2);
+      status = m_tuple->addItem("PiPluspxKalmanFit", m_PiPluspxKalmanFit);
+      status = m_tuple->addItem("PiPluspyKalmanFit", m_PiPluspyKalmanFit);
+      status = m_tuple->addItem("PiPluspzKalmanFit", m_PiPluspzKalmanFit);
+      status = m_tuple->addItem("PiPlusenergyKalmanFit", m_PiPlusenergyKalmanFit);
+      status = m_tuple->addItem("PiMinuspxKalmanFit", m_PiMinuspxKalmanFit);
+      status = m_tuple->addItem("PiMinuspyKalmanFit", m_PiMinuspyKalmanFit);
+      status = m_tuple->addItem("PiMinuspzKalmanFit", m_PiMinuspzKalmanFit);
+      status = m_tuple->addItem("PiMinusenergyKalmanFit", m_PiMinusenergyKalmanFit);
+      status = m_tuple->addItem("KSpxKalmanFit", m_KSpxKalmanFit);
+      status = m_tuple->addItem("KSpyKalmanFit", m_KSpyKalmanFit);
+      status = m_tuple->addItem("KSpzKalmanFit", m_KSpzKalmanFit);
+      status = m_tuple->addItem("KSenergyKalmanFit", m_KSenergyKalmanFit);
+      status = m_tuple->addItem("pipiKSFitSuccess", m_pipiKSFitSuccess);
+      status = m_tuple->addItem("pipiKSDecayLengthVeeVertex", m_pipiDecayLengthVeeVertex);
+      status = m_tuple->addItem("pipiKSChi2VeeVertex", m_pipiChi2VeeVertex);
+      status = m_tuple->addItem("pipiKSMassVeeVertex", m_pipiKSMassVeeVertex);
+      status = m_tuple->addItem("pipiKSDecayLengthFit", m_pipiDecayLengthFit);
+      status = m_tuple->addItem("pipiKSDecayLengthErrorFit", m_pipiDecayLengthErrorFit);
+      status = m_tuple->addItem("pipiKSChi2Fit", m_pipiChi2Fit);
+      status = m_tuple->addItem("pipiKSMassFit", m_pipiKSMassFit);
+    } else {
+      log << MSG::ERROR << "Cannot book NTuple for KSpipi Single Tags" << endmsg;
+      return StatusCode::FAILURE;
+    }
+    return StatusCode::SUCCESS;
+  }
+}
+
+StatusCode KSpipiSingleTag::execute() {
+  MsgStream log(msgSvc(), name());
+  log << MSG::INFO << "Executing KSpipi Single Tag Algorithm" << endreq;
+  SmartDataPtr<Event::EventHeader> eventHeader(eventSvc(), "/Event/EventHeader");
+  m_RunNumber = eventHeader->runNumber();
+  m_EventNumber = eventHeader->eventNumber();
+  DTagTool DTTool;
+  DTTool.setPID(true);
+  if(DTTool.isDTagListEmpty()) {
+    log << MSG::DEBUG << "No D candidates found" << endreq;
+    return StatusCode::SUCCESS;
+  }
+  if(!DTTool.cosmicandleptonVeto()) {
+    log << MSG::DEBUG << "Cosmic and lepton veto" << endreq;
+    return StatusCode::SUCCESS;
+  }
+  if(DTTool.findSTag(EvtRecDTag::kD0toKsPiPi)) {
+    DTagToolIterator DTTool_iter = DTTool.stag();
+    StatusCode FillTupleStatus = FillTuple(DTTool_iter, DTTool);
+    if(FillTupleStatus != StatusCode::SUCCESS) {
+      if(FillTupleStatus == StatusCode::RECOVERABLE) {
+	log << MSG::WARNING << "Vertex fit of KS failed, skipping event" << endreq;
+	return StatusCode::SUCCESS;
+      }
+      log << MSG::FATAL << "Assigning tuple info failed" << endreq;
+      return StatusCode::FAILURE;
+    }
+    m_tuple->write();
+  }
+  return StatusCode::SUCCESS;
+}
+
+StatusCode KSpipiSingleTag::finalize() {
+  MsgStream log(msgSvc(), name());
+  log << MSG::INFO << "Finalizing KSpipi Single Tagging" << endreq;
+  return StatusCode::SUCCESS;
+}
+
+StatusCode KSpipiSingleTag::FillTuple(DTagToolIterator DTTool_iter, DTagTool &DTTool) {
+  if(m_RunNumber < 0) {
+    SmartDataPtr<Event::McParticleCol> MCParticleCol(eventSvc(), "/Event/MC/McParticleCol");
+    if(!MCParticleCol) {
+      return StatusCode::FAILURE;
+    }
+    IMcDecayModeSvc *IMcDecayModeService;
+    StatusCode McDecayModeSVC_Status = service("McDecayModeSvc", IMcDecayModeService);
+    if(McDecayModeSVC_Status.isFailure()) {
+      return StatusCode::FAILURE;
+    }
+    FindMCInfo findMCInfo;
+    StatusCode MCStatus = findMCInfo.CalculateMCInfo(MCParticleCol, IMcDecayModeService);
+    if(MCStatus != StatusCode::SUCCESS) {
+      return MCStatus;
+    }
+    m_NumberParticles = findMCInfo.GetNumberParticles();
+    m_MCmode = findMCInfo.GetMCmode();
+    for(int i = 0; i < m_NumberParticles; i++) {
+      m_pdgID[i] = findMCInfo.GetpdgID(i);
+      m_MotherIndex[i] = findMCInfo.GetMotherIndex(i);
+      m_TruePx[i] = findMCInfo.GetTruePx(i);
+      m_TruePy[i] = findMCInfo.GetTruePy(i);
+      m_TruePz[i] = findMCInfo.GetTruePz(i);
+      m_TrueEnergy[i] = findMCInfo.GetTrueEnergy(i);
+    }
+  }
+  m_DMass = (*DTTool_iter)->mass();
+  m_MBC = (*DTTool_iter)->mBC();
+  m_DeltaE = (*DTTool_iter)->deltaE();
+  m_BeamE = (*DTTool_iter)->beamE();
+  m_Dpx = (*DTTool_iter)->p4().x();
+  m_Dpy = (*DTTool_iter)->p4().y();
+  m_Dpz = (*DTTool_iter)->p4().z();
+  m_Denergy = (*DTTool_iter)->p4().t();
+  FindKSpipiTagInfo findKSpipiTagInfo;
+  StatusCode status = findKSpipiTagInfo.CalculateTagInfo(DTTool_iter, DTTool);
+  if(status != StatusCode::SUCCESS) {
+    return status;
+  }
+  m_DecayLengthVeeVertex = findKSpipiTagInfo.GetDecayLengthVeeVertex();
+  m_Chi2VeeVertex = findKSpipiTagInfo.GetChi2VeeVertex();
+  m_KSMassVeeVertex = findKSpipiTagInfo.GetKSMassVeeVertex();
+  m_DecayLengthFit = findKSpipiTagInfo.GetDecayLengthFit();
+  m_DecayLengthErrorFit = findKSpipiTagInfo.GetDecayLengthErrorFit();
+  m_Chi2Fit = findKSpipiTagInfo.GetChi2Fit();
+  m_KSMassFit = findKSpipiTagInfo.GetKSMassFit();
+  m_KSPiPluspx = findKSpipiTagInfo.GetKSPiPlusP(0);
+  m_KSPiPluspy = findKSpipiTagInfo.GetKSPiPlusP(1);
+  m_KSPiPluspz = findKSpipiTagInfo.GetKSPiPlusP(2);
+  m_KSPiPlusenergy = findKSpipiTagInfo.GetKSPiPlusP(3);
+  m_KSPiMinuspx = findKSpipiTagInfo.GetKSPiMinusP(0);
+  m_KSPiMinuspy = findKSpipiTagInfo.GetKSPiMinusP(1);
+  m_KSPiMinuspz = findKSpipiTagInfo.GetKSPiMinusP(2);
+  m_KSPiMinusenergy = findKSpipiTagInfo.GetKSPiMinusP(3);
+  m_KSPiPluspxFit = findKSpipiTagInfo.GetKSPiPlusPFit(0);
+  m_KSPiPluspyFit = findKSpipiTagInfo.GetKSPiPlusPFit(1);
+  m_KSPiPluspzFit = findKSpipiTagInfo.GetKSPiPlusPFit(2);
+  m_KSPiPlusenergyFit = findKSpipiTagInfo.GetKSPiPlusPFit(3);
+  m_KSPiMinuspxFit = findKSpipiTagInfo.GetKSPiMinusPFit(0);
+  m_KSPiMinuspyFit = findKSpipiTagInfo.GetKSPiMinusPFit(1);
+  m_KSPiMinuspzFit = findKSpipiTagInfo.GetKSPiMinusPFit(2);
+  m_KSPiMinusenergyFit = findKSpipiTagInfo.GetKSPiMinusPFit(3);
+  m_KSpx = findKSpipiTagInfo.GetKShortP(0);
+  m_KSpy = findKSpipiTagInfo.GetKShortP(1);
+  m_KSpz = findKSpipiTagInfo.GetKShortP(2);
+  m_KSenergy = findKSpipiTagInfo.GetKShortP(3);
+  m_PiPluspx = findKSpipiTagInfo.GetPiPlusP(0);
+  m_PiPluspy = findKSpipiTagInfo.GetPiPlusP(1);
+  m_PiPluspz = findKSpipiTagInfo.GetPiPlusP(2);
+  m_PiPlusenergy = findKSpipiTagInfo.GetPiPlusP(3);
+  m_PiMinuspx = findKSpipiTagInfo.GetPiMinusP(0);
+  m_PiMinuspy = findKSpipiTagInfo.GetPiMinusP(1);
+  m_PiMinuspz = findKSpipiTagInfo.GetPiMinusP(2);
+  m_PiMinusenergy = findKSpipiTagInfo.GetPiMinusP(3);
+  m_KalmanFitSuccess = findKSpipiTagInfo.GetKalmanFitSuccess();
+  m_KalmanFitChi2 = findKSpipiTagInfo.GetKalmanFitChi2();
+  m_KSpxKalmanFit = findKSpipiTagInfo.GetKShortPKalmanFit(0);
+  m_KSpyKalmanFit = findKSpipiTagInfo.GetKShortPKalmanFit(1);
+  m_KSpzKalmanFit = findKSpipiTagInfo.GetKShortPKalmanFit(2);
+  m_KSenergyKalmanFit = findKSpipiTagInfo.GetKShortPKalmanFit(3);
+  m_PiPluspxKalmanFit = findKSpipiTagInfo.GetPiPlusPKalmanFit(0);
+  m_PiPluspyKalmanFit = findKSpipiTagInfo.GetPiPlusPKalmanFit(1);
+  m_PiPluspzKalmanFit = findKSpipiTagInfo.GetPiPlusPKalmanFit(2);
+  m_PiPlusenergyKalmanFit = findKSpipiTagInfo.GetPiPlusPKalmanFit(3);
+  m_PiMinuspxKalmanFit = findKSpipiTagInfo.GetPiMinusPKalmanFit(0);
+  m_PiMinuspyKalmanFit = findKSpipiTagInfo.GetPiMinusPKalmanFit(1);
+  m_PiMinuspzKalmanFit = findKSpipiTagInfo.GetPiMinusPKalmanFit(2);
+  m_pipiKSFitSuccess = findKSpipiTagInfo.GetpipiKSFitSuccess();
+  m_pipiDecayLengthVeeVertex = findKSpipiTagInfo.GetpipiDecayLengthVeeVertex();
+  m_pipiChi2VeeVertex = findKSpipiTagInfo.GetpipiChi2VeeVertex();
+  m_pipiKSMassVeeVertex = findKSpipiTagInfo.GetpipiKSMassVeeVertex();
+  m_pipiDecayLengthFit = findKSpipiTagInfo.GetpipiDecayLengthFit();
+  m_pipiDecayLengthErrorFit = findKSpipiTagInfo.GetpipiDecayLengthErrorFit();
+  m_pipiChi2Fit = findKSpipiTagInfo.GetpipiChi2Fit();
+  m_pipiKSMassFit = findKSpipiTagInfo.GetpipiKSMassFit();
+  return StatusCode::SUCCESS;
+}
