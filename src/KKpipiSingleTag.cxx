ce784d782e866e7239aa232f33715d1cc54d5a32
Martin Duy Tat <martin.tat@physics.ox.ac.uk>
2021-03-23 16:18:54 +0000

    Forgot an include

--- src/KKpipiSingleTag.cxx
+++ src/KKpipiSingleTag.cxx
@@ -4,6 +4,7 @@
 #include "KKpipi/KKpipiSingleTag.h"
 #include "KKpipi/FindKKpipiTagInfo.h"
 #include "KKpipi/FindMCInfo.h"
+#include "KKpipi/PIDTruth.h"
 // Gaudi
 #include "GaudiKernel/AlgFactory.h"
 #include "GaudiKernel/Bootstrap.h"
