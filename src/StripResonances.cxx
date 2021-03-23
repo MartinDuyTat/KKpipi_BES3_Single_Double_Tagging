5396aaa636310459d15dd6d807d4d7e201c20fad
Martin Duy Tat <martin.tat@physics.ox.ac.uk>
2021-03-23 18:05:15 +0000

    Written a class for removing all intermediate resonances in the MC truth PID array

---
+++ src/StripResonances.cxx
@@ -0,0 +1,42 @@
+// Martin Duy Tat 23rd March 2021
+
+// KKpipi
+#include "StripResonances.h"
+// STL
+#include <vector>
+#include <algorithm>
+
+StripResonances::StripResonances(const std::vector<int> &KeepList) {
+  m_KeepList = std::vector<int>{30443, 421, 130, 310, 321, 111, 211, 11, 12, 13, 14, 22, 2212};
+  m_KeepList.insert(m_KeepList.end(), KeepList.begin(), KeepList.end());
+}
+
+int StripResonances::FindResonanceIndex(const std::vector<int> &pidID) const {
+  for(unsigned int i = 0; i < pidID.size(); i++) {
+    if(std::find(m_KeepList.begin(), m_KeepList.end(), pidID[i] == m_KeepList.end())) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+void RemoveResonance(int ResonanceIndex, std::vector<int> &pidID, std::vector<int> &MotherIndex) const {
+  int Mother = MotherIndex[ResonanceIndex];
+  for(unsigned int i = 0; i < pidID.size(); i++) {
+    if(MotherIndex[i] == ResonanceIndex) {
+      MotherIndex[i] = Mother;
+    } else if (MotherIndex[i] > ResonanceIndex) {
+      MotherIndex[i]--;
+    }
+  }
+  pidID.erase(pidID.begin() + ResonanceIndex);
+  MotherIndex.erase(MotherIndex.begin() + ResonanceIndex);
+}
+
+void RemoveIntermediateResonances(std::vector<int> &pidID, std::vector<int> &MotherIndex) const {
+  int ResonanceIndex = FindResonanceIndex(pidID);
+  while(ResonanceIndex != -1) {
+    RemoveResonance(pidID, MotherIndex);
+    ResonanceIndex = FindResonanceIndex(pidID);
+  }
+}
