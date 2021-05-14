260d66b91b5de15827e97a754ba13cf841f680be
Martin Duy Tat <martin.tat@physics.ox.ac.uk>
2021-05-14 22:45:00 +0100

    Fixed some double to int for charges

--- KKpipi/KKpipiVersusKpiDoubleTag.h
+++ KKpipi/KKpipiVersusKpiDoubleTag.h
@@ -385,7 +385,7 @@ class KKpipiVersusKpiDoubleTag: public Algorithm {
     /**
      * Tag \f$\pi\f$ charge
      */
-    NTuple::Item<double> m_TagPiCharge;
+    NTuple::Item<int> m_TagPiCharge;
     /**
      * Tag \f$K\f$ momentum along \f$x\f$
      */
@@ -405,7 +405,7 @@ class KKpipiVersusKpiDoubleTag: public Algorithm {
     /**
      * Tag \f$K\f$ charge
      */
-    NTuple::Item<double> m_TagKCharge;
+    NTuple::Item<int> m_TagKCharge;
     /**
      * Equal to 1 if the tag daughter tracks are from the same \f$D\f$ meson
      */
